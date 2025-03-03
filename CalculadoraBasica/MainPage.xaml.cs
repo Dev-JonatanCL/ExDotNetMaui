e6bf037a0f0af611449164589fa726bd98d4c4fa
Jonatan C. Luz <dev.jonatancl@gmail.com>
2025-03-03 11:00:53 -0300

    Add files via upload

---
+++ CalculadoraBasica/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculadoraBasica
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
