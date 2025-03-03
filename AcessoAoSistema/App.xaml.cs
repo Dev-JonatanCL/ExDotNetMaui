2471bf1cefb0d59d150cd4cf73021a0f50e6c6e9
Jonatan C. Luz <dev.jonatancl@gmail.com>
2025-03-03 10:54:54 -0300

    Add files via upload

---
+++ AcessoAoSistema/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AcessoAoSistema
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AcessoAoSistema/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AcessoAoSistema
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
