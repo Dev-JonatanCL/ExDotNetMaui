188cd01488fb9e3539e6d991c9f6c8ba163d5c28
Jonatan C. Luz <dev.jonatancl@gmail.com>
2025-03-03 10:59:32 -0300

    Add files via upload

---
+++ CadastroAlunos/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CadastroAlunos
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
